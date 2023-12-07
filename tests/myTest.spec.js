1467e694e94b9d3ca9cac5a2772d56c274932343
Tasha Miller <popelyshkon25@gmail.com>
2023-12-07 22:54:55 +0200

    Fixed comments

--- tests/myTest.spec.js
+++ tests/myTest.spec.js
@@ -1,30 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('Perform Login', async ({ page }) => {
-  
+test.beforeEach(async ({ page }, testInfo) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
+});
 
+test('Perform Login', async ({ page }) => {
   await expect(page.getByText('Products')).toBeVisible();
   await expect(page.locator('#shopping_cart_container a')).toBeVisible();
 
   const inventoryItems = await page.locator(".inventory_item");
-  expect(inventoryItems).toHaveCount(6);
+  expect(inventoryItems).toBeGreaterThan(6);
 });
 
 test('Add product to the cart', async ({ page }) => {
-  
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-
   //add item to a cart
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await expect(page.locator('#shopping_cart_container > a > span')).toContainText('1');
